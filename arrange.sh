ff97d4be165901579673c0df108fb32566cc828d
EunjeLee0812 <99636075+EunjeLee0812@users.noreply.github.com>
2025-04-09 21:38:05 +0900

    Add files via upload

--- arrange.sh
+++ arrange.sh
@@ -1,23 +1,6 @@
-#!/bin/bash
+for file in files/*; do    
+    first_char=$(basename "$file" | cut -c1 | tr '[:upper:]' '[:lower:]')
 
-# Check the number of arguments (we need at least one).
-if [ $# -lt 1 ]; then
-    echo "Usage: $0 [arguments]"
-    exit 1
-fi
-
-# Show basic information about the arguments provided
-echo "First argument: $1"
-echo "Second argument: $2"
-echo "Total number of arguments: $#"
-echo "All arguments: $@"
-
-# Check if the first argument is a directory
-if [ -d "$1" ]; then
-    echo "\"$1\" is a directory."
-else
-    echo "\"$1\" is not a directory."
-fi
-
-# Exit cleanly
-exit 0
+    
+    mv "$file" "$first_char"/
+done
